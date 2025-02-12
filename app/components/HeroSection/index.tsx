a6497afa75fc90a107073ff9b54593e53ed734ad
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2025-02-12 16:45:33 -0300

    chore: develop the home page of the softeam hero section with the font and logo

--- app/components/HeroSection/index.tsx
+++ app/components/HeroSection/index.tsx
@@ -1,13 +1,21 @@
+import { Poppins } from "next/font/google";
 import Image from "next/image";
 import React from "react";
 import lamp from "../../../assets/lamp.png";
+import softeamLogo from "../../../assets/logo-softeam.png";
+
+const poppins = Poppins({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 function HeroSection(): React.JSX.Element {
   return (
     <main className="w-full h-full flex flex-col bg-color-3">
-      <section className="w-full h-full px-28 flex flex-col">
-        <div className="w-full h-4/5">
-          <div className="text-white text-5xl flex flex-col gap-y-3">
+      <section className="w-full h-full px-32 flex flex-col">
+        <div className="w-full h-full flex items-center justify-between">
+          <div className={`text-white text-7xl flex flex-col gap-y-1 ${poppins.className}`}>
             <article className="flex gap-x-3">
               <p>Nós</p>
               <p className="font-bold">transformamos</p>
@@ -18,12 +26,16 @@ function HeroSection(): React.JSX.Element {
               <p className="font bold">ideias</p>
               <p>em</p>
             </article>
-            <p className="font-bold">realidade</p>
+            <p className="font-bold mb-3">realidade</p>
+            <p className="w-2/3 text-xl text-wrap tracking-widest">
+              Desenvolvemos soluções de qualidade com o maior custo benefício do mercado para fazer seu projeto decolar!
+            </p>
           </div>
-          {/* <Image alt="softeam-logo" src={softeamLogo} width={34} height={34} /> */}
+          <Image alt="softeam-logo" src={softeamLogo} className="w-auto h-3/5" />
         </div>
       </section>
     </main>
   );
 }
+
 export default HeroSection;
