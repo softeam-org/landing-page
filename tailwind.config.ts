dcf7f5d36f72194483e3a599638cdc1a68588c16
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2025-02-12 09:04:11 -0300

    feat: create bg and color palette taiwlindcss custom classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,20 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        // This is the color palette tailwindcss
+        // classes of the softeam landing page:
+        ["color-1"]: "#072763",
+        ["color-2"]: "#1c58a8",
+        ["color-3"]: "#1074bc",
+        ["color-4"]: "#94d2ff",
+      },
+      backgroundColor: {
+        // This is the background color palette tailwindcss
+        // classes of the softeam landing page:
+        ["color-1"]: "#072763",
+        ["color-2"]: "#1c58a8",
+        ["color-3"]: "#1074bc",
+        ["color-4"]: "#94d2ff",
       },
     },
   },
