2890a9a7bc3cfc95352ba3a1d734762e3f233bbe
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2025-02-12 11:45:21 -0300

    chore: add the hero section component in the main app page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import HeroSection from "./components/HeroSection";
 
-function Home(): React.JSX.Element {
+function App(): React.JSX.Element {
   return (
-    <div className="">
-      <div></div>
+    <div className="w-screen min-h-screen h-screen flex flex-col">
+      <div className="h-24 w-full"></div>
+      <HeroSection />
     </div>
   );
 }
 
-export default Home;
+export default App;
