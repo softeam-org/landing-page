be7cb8a29ef6820d6f1fd72bc682226e096e34b7
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2025-02-12 22:26:55 -0300

    feat: import open sans font family and fix the 2nd paragraph font to it

--- app/components/HeroSection/index.tsx
+++ app/components/HeroSection/index.tsx
@@ -1,11 +1,17 @@
-import { Poppins } from "next/font/google";
+import { Open_Sans, Poppins } from "next/font/google";
 import Image from "next/image";
 import React from "react";
 import lamp from "../../../assets/lamp.png";
-import softeamLogo from "../../../assets/logo-softeam.png";
+import softeamLogo from "../../../assets/logo-softeam.svg";
 
 const poppins = Poppins({
   weight: ["400", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const openSans = Open_Sans({
+  weight: ["400", "500", "700"],
   subsets: ["latin"],
   display: "swap",
 });
@@ -22,16 +28,16 @@ function HeroSection(): React.JSX.Element {
             </article>
             <article className="flex gap-3">
               <p>suas</p>
-              <Image alt="softeam-logo" src={lamp} className="w-fit" />
+              <Image alt="softeam-logo" src={lamp} />
               <p className="font bold">ideias</p>
               <p>em</p>
             </article>
             <p className="font-bold mb-3">realidade</p>
-            <p className="w-2/3 text-xl text-wrap tracking-widest">
+            <p className={`w-2/3 text-2xl font-medium tracking-wider ${openSans.className}`}>
               Desenvolvemos soluções de qualidade com o maior custo benefício do mercado para fazer seu projeto decolar!
             </p>
           </div>
-          <Image alt="softeam-logo" src={softeamLogo} className="w-auto h-3/5" />
+          <Image alt="softeam-logo" src={softeamLogo} className="w-auto h-1/2" />
         </div>
       </section>
     </main>
