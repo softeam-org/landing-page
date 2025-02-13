89df31e0fccca403c771d96af0dcfd8f40d63353
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2025-02-13 11:09:01 -0300

    chore: add min height on hero section and add waves animation

--- app/components/HeroSection/index.tsx
+++ app/components/HeroSection/index.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import React from "react";
 import lamp from "../../../assets/lamp.png";
 import softeamLogo from "../../../assets/logo-softeam.svg";
+import Waves from "./waves";
 
 const poppins = Poppins({
   weight: ["400", "700"],
@@ -18,8 +19,8 @@ const openSans = Open_Sans({
 
 function HeroSection(): React.JSX.Element {
   return (
-    <main className="w-full h-full flex flex-col bg-color-3">
-      <section className="w-full h-full px-32 flex flex-col">
+    <main className="w-full min-h-screen flex flex-col pt-32 overflow-x-hidden overflow-y-auto bg-color-3">
+      <section className="w-full px-32 flex flex-col">
         <div className="w-full h-full flex items-center justify-between">
           <div className={`text-white text-7xl flex flex-col gap-y-1 ${poppins.className}`}>
             <article className="flex gap-x-3">
@@ -28,7 +29,7 @@ function HeroSection(): React.JSX.Element {
             </article>
             <article className="flex gap-3">
               <p>suas</p>
-              <Image alt="softeam-logo" src={lamp} />
+              <Image alt="lamp" src={lamp} />
               <p className="font bold">ideias</p>
               <p>em</p>
             </article>
@@ -37,9 +38,10 @@ function HeroSection(): React.JSX.Element {
               Desenvolvemos soluções de qualidade com o maior custo benefício do mercado para fazer seu projeto decolar!
             </p>
           </div>
-          <Image alt="softeam-logo" src={softeamLogo} className="w-auto h-1/2" />
+          <Image alt="softeam-logo" src={softeamLogo} className="w-auto h-7/12" />
         </div>
       </section>
+      <Waves />
     </main>
   );
 }
