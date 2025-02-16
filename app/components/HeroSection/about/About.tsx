b46b4c17e74e9a05eb1cf85adbd8eee019756722
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2025-02-15 22:20:58 -0300

    feat: add the "quem somos" title and paragraphs

--- app/components/HeroSection/about/About.tsx
+++ app/components/HeroSection/about/About.tsx
@@ -1,6 +1,24 @@
 import React from "react";
 
 function About(): React.JSX.Element {
-  return <section className="w-full min-h-screen bg-red-500"></section>;
+  return (
+    <section className="w-full min-h-screen bg-color-1">
+      <article className="text-white">
+        <h2>QUEM SOMOS</h2>
+        <div>
+          <p>
+            Fundada em 1997, a SofTeam é uma empresa júnior, que entrega soluções digitais abrangendo diversas áreas da
+            computação. Composta pelos cursos do Departamento de Computação da Universidade Federal de Sergipe, a
+            instituição de ensino mais relevante do estado, buscamos apresentar criatividade e inovação em nossos
+            trabalhos, proporcionando soluções de alto valor agregado.
+          </p>
+          <p>
+            Nossos serviços são desenvolvidos pensando no cliente. A partir dos desafios que recebemos, criamos soluções
+            que visam trazer o melhor retorno possível.
+          </p>
+        </div>
+      </article>
+    </section>
+  );
 }
 export default About;
