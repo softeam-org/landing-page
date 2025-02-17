5ddb9f58b02137fe534717d6cd3696385765146c
Thiago Menezes <thiagomvas@gmail.com>
2025-02-17 19:25:08 -0300

    fix: Add missing line accidentally deleted from merge conflict

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,13 @@ import HeroSection from "./components/HeroSection/HeroSection";
 import Image from "next/image";
 import  Footer  from "./components/footer/footer";
 
+function App(): React.JSX.Element {
   return (
     <div className="w-full min-h-screen flex flex-col overflow-x-hidden">
       <div className="w-full h-24 fixed"></div>
       <HeroSection />
+      <Footer />
     </div>
-    
-    
-    <Footer />
   );
 }
 
