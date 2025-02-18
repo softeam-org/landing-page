69fee6f0d33bcdb01af260479f8fe9791dd982c7
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2025-02-17 23:18:42 -0300

    chore: finish the hero section responsivity

--- app/components/HeroSection/waves/Waves.tsx
+++ app/components/HeroSection/waves/Waves.tsx
@@ -2,7 +2,14 @@ import React from "react";
 import Wave from "./Wave";
 import "./waves.css";
 
-function Waves({ d, fill, bg }: { d: string; fill: string; bg?: string }): React.JSX.Element {
+interface WavesProps {
+  d: string;
+  fill: string;
+  bg?: string;
+  hClass: string;
+}
+
+function Waves({ d, fill, bg, hClass }: WavesProps): React.JSX.Element {
   // JavaScript object to style the waves:
   const waveStyle: React.CSSProperties = {
     top: "0",
@@ -25,8 +32,8 @@ function Waves({ d, fill, bg }: { d: string; fill: string; bg?: string }): React
 
   return (
     <div
-      className={`relative flex items-start mt-auto h-40 max-md:h-32 overflow-hidden ${bg ? bg : ""}`}
-      style={{ width: "200vw" }}
+      className={`relative flex items-start mt-auto ${hClass}  overflow-hidden ${bg ? bg : ""}`}
+      style={{ width: "150vw" }}
     >
       {arr.map((value, index: number) => {
         const opcty = calculateWaveOpacity(index, 4);
