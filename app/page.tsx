5ba773347a9433a54b9237688014e05c9851480b
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2025-02-17 22:39:51 -0300

    fix: recreate the erased app react component

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,14 @@
 import React from "react";
+import Footer from "./components/footer/footer";
 import HeroSection from "./components/HeroSection/HeroSection";
-import Image from "next/image";
-import  Footer  from "./components/footer/footer";
 
+function App(): React.JSX.Element {
   return (
     <div className="w-full min-h-screen flex flex-col overflow-x-hidden">
       <div className="w-full h-24 fixed"></div>
       <HeroSection />
+      <Footer />
     </div>
-    
-    
-    <Footer />
   );
 }
 
