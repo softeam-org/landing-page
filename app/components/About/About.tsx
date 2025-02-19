fa98eec7c56dffd92173313c7bc077b5fb0400bd
gbatistaa <gbatistadev@gmail.com>
2025-02-19 19:59:22 +0000

    chore: create and style the softeam values cards

--- app/components/About/About.tsx
+++ app/components/About/About.tsx
@@ -1,29 +1,35 @@
 import { openSans, poppins } from "@/app/ts/fonts";
 import React from "react";
+import { AiTwotoneAlert } from "react-icons/ai";
+import { FaHandHoldingHeart, FaRegCalendarCheck } from "react-icons/fa";
 import { PiSunHorizonDuotone, PiTarget } from "react-icons/pi";
+
 import MissionVisionCard from "./MissionVision/MissionVisionCard";
+import ValueCard from "./Values/ValueCard";
 
 function About(): React.JSX.Element {
   return (
-    <section className="w-full min-h-screen flex flex-col gap-12 px-44 pt-12 text-white bg-color-1">
+    <section
+      className={`w-full min-h-screen flex flex-col gap-16 px-44 pt-12 ${poppins.className} text-white bg-color-1`}
+    >
       {/* Quem Somos */}
       <article className="flex flex-col gap-6">
         <h2 className={`font-bold text-2xl tracking-wide ${openSans.className}`}>QUEM SOMOS</h2>
         <div className="flex flex-col gap-3">
-          <p className={`text-lg font-light ${poppins.className}`}>
+          <p className={"text-lg font-light "}>
             Fundada em 1997, a SofTeam é uma empresa júnior, que entrega soluções digitais abrangendo diversas áreas da
             computação. Composta pelos cursos do Departamento de Computação da Universidade Federal de Sergipe, a
             instituição de ensino mais relevante do estado, buscamos apresentar criatividade e inovação em nossos
             trabalhos, proporcionando soluções de alto valor agregado.
           </p>
-          <p className={`text-lg font-light ${poppins.className}`}>
+          <p className={"text-lg font-light"}>
             Nossos serviços são desenvolvidos pensando no cliente. A partir dos desafios que recebemos, criamos soluções
             que visam trazer o melhor retorno possível.
           </p>
         </div>
       </article>
       {/* Missão/Visão */}
-      <article className="grid grid-cols-2 items-stretch justify-items-center gap-10">
+      <article className="grid grid-cols-2 items-stretch justify-items-center">
         <MissionVisionCard
           misVisName="MISSÃO"
           misVisText="Formar profissionais diferenciados e líderes preparados,
@@ -38,6 +44,20 @@ function About(): React.JSX.Element {
             preparados para transformar o mercado."
           misVisIcon={PiSunHorizonDuotone}
         />
+      </article>
+      <article>
+        <p className="text-center">Nossos serviços são desenvolvidos pensando no cliente.</p>
+        <p className="text-center">
+          A partir dos desafios que recebemos, criamos soluções que visam trazer o melhor retorno possível.
+        </p>
+      </article>
+      <article className="flex flex-col gap-10">
+        <h3 className="text-2xl text-center font-semibold">VALORES</h3>
+        <div className="grid grid-cols-3 items-stretch gap-10">
+          <ValueCard valueIcon={FaHandHoldingHeart} valueName="Paixão pelo que fazemos" />
+          <ValueCard valueIcon={FaRegCalendarCheck} valueName="Compromisso com resultados" />
+          <ValueCard valueIcon={AiTwotoneAlert} valueName="Autocrítica com nossas soluções" />
+        </div>
       </article>
     </section>
   );
