d5019cd33b8a576009990842a41aecb31c9c5e4c
gbatistaa <gbatistadev@gmail.com>
2025-02-19 19:59:47 +0000

    feat: create the softeam value card component

--- app/components/About/Values/ValueCard.tsx
+++ app/components/About/Values/ValueCard.tsx
@@ -4,19 +4,15 @@ import { IconType } from "react-icons";
 // Interface for the softeam values cards components:
 interface ValueCardProps {
   valueName: string;
-  valueText: string;
   valueIcon: IconType;
 }
 
-// Softeam values cards components:
-function ValueCard({ valueName, valueText, valueIcon: Icon }: ValueCardProps): React.JSX.Element {
+// Softeam values cards components
+function ValueCard({ valueName, valueIcon: Icon }: ValueCardProps): React.JSX.Element {
   return (
-    <div>
-      <div>
-        <Icon />
-        <p>{valueName}</p>
-      </div>
-      <p>{valueText}</p>
+    <div className="flex flex-col items-center px-10 py-14 gap-4 rounded-md cursor-pointer bg-color-2">
+      <Icon className="w-auto h-8 mb-2" />
+      <p className="font-semibold text-2xl text-center">{valueName}</p>
     </div>
   );
 }
