1dfdc9cf212a41f45d77d69514637d5c25eba2ec
Thiago Menezes <thiagomvas@gmail.com>
2025-02-20 20:49:56 -0300

    feat: Add navbar to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Footer from "./components/footer/footer";
 import HeroSection from "./components/HeroSection/HeroSection";
+import Navbar from "./components/Navbar/index"
 
 function App(): React.JSX.Element {
   return (
     <div className="w-full min-h-screen flex flex-col overflow-x-hidden">
+
       <div className="w-full h-24 fixed"></div>
+    <Navbar/>
+
       <HeroSection />
       <Footer />
     </div>
