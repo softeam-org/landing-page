c1ab0e740e53b83cf5c58fcaaedca1ea74d3c783
gbatistaa <gbatistadev@gmail.com>
2025-02-20 14:16:01 +0000

    chore: complete responsivity of the mission/vision cards component

--- app/components/About/MissionVision/MissionVisionCard.tsx
+++ app/components/About/MissionVision/MissionVisionCard.tsx
@@ -11,14 +11,14 @@ interface MisVisCardProps {
 function MissionVisionCard({ misVisName, misVisText, misVisIcon: Icon }: MisVisCardProps): React.JSX.Element {
   return (
     <div
-      className={`flex flex-col w-2/3 p-10 gap-4 shadow-[0_0_5px_1px_#114e7e] ease-out duration-200
+      className={`flex flex-col w-7/10 max-xl:w-4/5 max-lg:w-11/12 max-md:w-full p-10 max-xl:px-10 max-xl:py-12 max-md:py-8 gap-4 shadow-[0_0_5px_1px_#114e7e] ease-out duration-200
        rounded-md cursor-pointer ${poppins.className} hover:bg-color-2`}
     >
       <div className="flex w-fit items-center gap-2 justify-center">
         <Icon className="w-auto h-10" />
         <h3 className="text-2xl font-bold">{misVisName}</h3>
       </div>
-      <p className="text-sm font-medium tracking-wide">{misVisText}</p>
+      <p className="text-sm font-medium tracking-wide max-xl:tracking-wider">{misVisText}</p>
     </div>
   );
 }
