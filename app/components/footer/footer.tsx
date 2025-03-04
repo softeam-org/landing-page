23e295d51b0778f13fb958f81ebbd7e297af87b8
edDEVjr <edvaldodossantos737@gmail.com>
2025-03-04 16:25:56 -0300

    feat:updated alt tag on all images

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -5,20 +5,20 @@ import "./footer.css";
 const textBotton = "Software Team 2025 \u00A9 :: Feito com Café e Paçoca";
 
 const information = [
-  { icon: "/email.png", text: "contato@softeam.com.br" },
-  { icon: "/location.png", text: "Av. Marechal Rondon, S/N | Departamento de Computacao - UFS" },
-  { icon: "/phone.png", text: "+55 79 8100-0041" },
+  { icon: "/email.png",alt:"Icone representando um email", text: "contato@softeam.com.br" },
+  { icon: "/location.png",alt:"Icone representando uma Localização", text: "Av. Marechal Rondon, S/N | Departamento de Computacao - UFS" },
+  { icon: "/phone.png",alt:"Icone representando um Telefone" ,text: "+55 79 8100-0041" },
 ];
 
 const socialNetworks = [
-  { icon: "/instagram.png", name: "Instagram", link: "https://www.instagram.com/softeam.ejc/", effectHover: "linkSN1" },
+  { icon: "/instagram.png", alt: "Logo do Instagram", link: "https://www.instagram.com/softeam.ejc/", effectHover: "linkSN1" },
   {
     icon: "/linkedin.png",
-    name: "Linkedin",
+    alt: "Logo do Linkedin",
     link: "https://www.linkedin.com/company/softeam-ejc",
     effectHover: "linkSN2",
   },
-  { icon: "/whatsapp.png", name: "Whatsapp", link: "https://wa.me/+557981000041", effectHover: "linkSN3" },
+  { icon: "/whatsapp.png", alt: "Logo do Whatsapp", link: "https://wa.me/+557981000041", effectHover: "linkSN3" },
 ];
 
 function Footer(): React.JSX.Element {
@@ -28,7 +28,6 @@ function Footer(): React.JSX.Element {
         {information.map((info, index) => (
           <div className="info" key={index}>
             <div className="iconInfo">
-              {/* Debug: Check if the image loads */}
               <Image src={info.icon} alt={info.text} width={25} height={25} />
             </div>
             <p>{info.text}</p>
@@ -39,7 +38,7 @@ function Footer(): React.JSX.Element {
       <div className="socialNetworks">
         {socialNetworks.map((SN, index) => (
           <a className={SN.effectHover} key={index} href={SN.link} target="_blank" rel="noopener noreferrer">
-            <Image className="iconSN" src={SN.icon} alt={SN.name} width={40} height={40} />
+            <Image className="iconSN" src={SN.icon} alt={SN.alt} width={40} height={40} />
           </a>
         ))}
       </div>
