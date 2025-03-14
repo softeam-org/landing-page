4105fa25e8437e0e306aa9cbef3d2f78c7530aaa
edDEVjr <edvaldodossantos737@gmail.com>
2025-03-14 14:34:12 -0300

    Add main texts

--- app/ps/ps.tsx
+++ app/ps/ps.tsx
@@ -4,9 +4,29 @@ function PS(): ReactElement {
     return (
         <div className="container">
             <div className="about">
-                <div className="text"></div>
+                <div className="text">
+                    <h1>Faça parte da maior empresa Júnior de Sergipe</h1>
+                    <p>
+                        Fundada em 1997, a SofTeam é uma empresa sem fins lucrativos que entrega soluções digitais,
+                        abrangendo diversas áreas da computação. Composta pelos cursos do Departamento de Computação
+                        da Universidade Federal de Sergipe, tem como um de seus objetivos capacitar e aperfeiçoar o
+                        desenvolvimento dos alunos do DCOMP, proporcionando vivência empresarial e a prática com o
+                        que há de atual no mercado.
+                    </p>
+
+                </div>
                 <div className="image"></div>
-                <div className="text2"></div>
+                
+                <div className="text2">
+                    <h2>Por que entrar na SofTeam?</h2>
+                    <p>
+                        Seja entrando em contato e negociando com clientes reais, pondo em prática o que estudou em sala
+                    de aula para resolver problemas reais ou aprendendo, na prática, habilidades de gestão, a SofTeam
+                    proporciona à estudantes dos cursos do DCOMP a oportunidade de se desenvolver profissionalmente e
+                    ir além da grade curricular. Muito mais que isso, a SofTeam oferece proximidade com o mercado e
+                    interação com empresas e empresários júniores de diversas áreas e estados.
+                    </p>
+                </div>
             </div>
             
             
