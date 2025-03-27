256c76f50fc56e59ec81bfac08b2fde56d412bb7
Gabriel Barbosa <gbatistadev@gmail.com>
2025-03-27 20:54:50 -0300

    chore: remove the useless image component and transform in a export array image file

---
+++ app/ts/parternsPNGs.ts
@@ -0,0 +1,6 @@
+import { StaticImageData } from "next/image";
+import img1 from "./image1.png";
+import img2 from "./image2.png";
+import img3 from "./image3.png";
+
+export const images: StaticImageData[] = [img1, img2, img3];
