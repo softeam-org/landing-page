a98c346e54c99397409dc5a12b94e68ac1f27208
Gabriel Barbosa <gbatistadev@gmail.com>
2025-03-27 20:52:03 -0300

    chore: remove single quotes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,9 +10,9 @@ export default {
     extend: {
       colors: {
         "blue-94D2FF": "#94D2FF",
-        'blue-1074BC': '#1074BC',
-        'blue-1C58A8': '#1C58A8',
-        'blue-072763': '#072763',
+        "blue-1074BC": "#1074BC",
+        "blue-1C58A8": "#1C58A8",
+        "blue-072763": "#072763",
         background: "var(--background)",
         foreground: "var(--foreground)",
         // This is the color palette tailwindcss
