bfe91483373792e2d4e6ff30afc419e7dc89c592
Gabriel Barbosa <gbatistadev@gmail.com>
2025-03-27 22:43:56 -0300

    chore: change the paths of the partners carroussel images

--- app/ts/parternsPNGs.ts
+++ app/ts/parternsPNGs.ts
@@ -1,6 +1,6 @@
 import { StaticImageData } from "next/image";
-import img1 from "../components/Parceiros/image1.png";
-import img2 from "../components/Parceiros/image2.png";
-import img3 from "../components/Parceiros/image3.png";
+import img1 from "../../public/image1.png";
+import img2 from "../../public/image2.png";
+import img3 from "../../public/image3.png";
 
 export const images: StaticImageData[] = [img1, img2, img3];
