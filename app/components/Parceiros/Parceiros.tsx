a6a64f5a473e52483c1e9aca55bed619bc8a584d
Gabriel Barbosa <gbatistadev@gmail.com>
2025-03-27 22:44:13 -0300

    feat: finish the infinite fluid carroussel

--- app/components/Parceiros/Parceiros.tsx
+++ app/components/Parceiros/Parceiros.tsx
@@ -3,42 +3,42 @@
 import { poppins } from "@/app/ts/fonts";
 import Image from "next/image";
 import React from "react";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import { Autoplay, FreeMode, Navigation, Pagination } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
 import { images } from "../../ts/parternsPNGs";
 
 function Partners(): React.JSX.Element {
+  const settings = {
+    infinite: true,
+    speed: 3000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 0,
+    cssEase: "linear",
+    arrows: false,
+  };
+
   return (
     <section
-      className="w-full flex flex-col gap-16 px-44 max-xl:px-36 max-lg:px-16 max-md:px-10 max-xs:px-14
+      className="relative w-full flex flex-col gap-16 px-44 max-xl:px-36 max-lg:px-16 max-md:px-10 max-xs:px-14
       py-12 bg-color-5"
     >
       <h3 className={`text-2xl text-center font-semibold ${poppins.className}`}>NOSSOS PARCEIROS</h3>
-      <Swiper
-        modules={[Navigation, Pagination, Autoplay, FreeMode]}
-        navigation
-        freeMode={true}
-        slidesPerView={1}
-        spaceBetween={10}
-        autoplay={{ delay: 300, disableOnInteraction: false, pauseOnMouseEnter: false }}
-        loop={true}
-        speed={3000}
-        className="flex w-1/2 h-64 rounded-lg "
-      >
-        {images.map((src, index) => (
-          <SwiperSlide key={index} className="relative flex items-center justify-center">
-            <Image
-              src={src}
-              className="swiper-lazy object-cover w-64 absolute inset-0 m-auto aspect-square rounded-full"
-              alt={`Parceiro ${index + 1}`}
-            />
-            <div className="swiper-lazy-preloader"></div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <div className="relative">
+        <div className="absolute z-30 flex justify-between h-64 w-full">
+          <div className="h-full w-10 bg-gradient-to-r from-color-5 from-20% to-transparent"></div>
+          <div className="h-full w-10 bg-gradient-to-l from-color-5 from-20% to-transparent"></div>
+        </div>
+        <Slider {...settings} className="z-20 flex w-full h-64">
+          {images.map((src, index) => (
+            <div key={index} className="flex items-center justify-center">
+              <Image src={src} className="object-cover w-64 aspect-square" alt={`Parceiro ${index + 1}`} />
+            </div>
+          ))}
+        </Slider>
+      </div>
     </section>
   );
 }
