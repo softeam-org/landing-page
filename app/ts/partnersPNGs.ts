9ee502089faa511edafe0e022b0f35de9b860e9e
Thiago Vasconcelos <47324415+thiagomvas@users.noreply.github.com>
2026-01-26 17:28:52 -0300

    chore: Remove partner

--- app/ts/partnersPNGs.ts
+++ app/ts/partnersPNGs.ts
@@ -1,6 +1,5 @@
 import brasilJunior from "@/public/brasil-junior.png";
-import innovationhub from "@/public/logo-ih.png";
 import serjunior from "@/public/serjunior.webp";
 import { StaticImageData } from "next/image";
 
-export const images: StaticImageData[] = [innovationhub, serjunior, brasilJunior];
+export const images: StaticImageData[] = [serjunior, brasilJunior];
